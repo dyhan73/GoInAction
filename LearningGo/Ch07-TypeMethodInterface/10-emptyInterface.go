eb82d873bda54e0b51a86b99cc16a876bed3c1e1
dyhan <dyhan@sk.com>
2022-12-29 13:06:00 +0900

    ch7.10

---
+++ LearningGo/Ch07-TypeMethodInterface/10-emptyInterface.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+type LinkedList struct {
+	Value interface{}
+	Next  *LinkedList
+}
+
+func (ll *LinkedList) Insert(pos int, val interface{}) *LinkedList {
+	if ll == nil || pos == 0 {
+		return &LinkedList{
+			Value: val,
+			Next:  ll,
+		}
+	}
+	ll.Next = ll.Next.Insert(pos-1, val)
+	return ll
+}
+
+func main() {
+	l := &LinkedList{
+		Value: "KK",
+		Next:  nil,
+	}
+	fmt.Println(l)
+	l = l.Insert(2, "ka1")
+	fmt.Println(l, l.Next)
+	l = l.Insert(2, "ka2")
+	fmt.Println(l, l.Next, l.Next.Next)
+	l = l.Insert(1, "ka3")
+	fmt.Println(l, l.Next, l.Next.Next, l.Next.Next.Next)
+}
